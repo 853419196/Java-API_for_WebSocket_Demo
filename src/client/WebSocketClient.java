70c78dcd6a90309b251f5484d03926606cb3dda4
砂漠の毒虫 <853419196@qq.com>
2019-09-15 12:27:46 +0900

    Update WebSocketClient.java

--- src/client/WebSocketClient.java
+++ src/client/WebSocketClient.java
@@ -2,21 +2,25 @@ package client;
 import javax.websocket.*;
 @ClientEndpoint public class WebSocketClient
 {
-    @OnClose public void onClose()
+    @OnClose
+    public void onClose()
     {
         System.out.println("WebSocket Client: onClose()");
     }
-    @OnError public void onError(Throwable throwable)
+    @OnError
+    public void onError(Throwable throwable)
     {
         System.out.println("WebSocket Client: onError()");
         throwable.printStackTrace();
     }
-    @OnMessage public void onMessage(String string)
+    @OnMessage
+    public void onMessage(String string)
     {
         System.out.println("WebSocket Client: onMessage()");
         System.out.println("WebSocket Server: "+string);
     }
-    @OnOpen public void onOpen()
+    @OnOpen
+    public void onOpen()
     {
         System.out.println("WebSocket Client: onOpen()");
     }
