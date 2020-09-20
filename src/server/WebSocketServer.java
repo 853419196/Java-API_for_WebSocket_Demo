bce82728be7953b2479a3afcbb76bfb11ca5d3fe
砂漠の毒虫 <853419196@qq.com>
2020-09-20 14:31:22 +0900

    Add files via upload

---
+++ src/server/WebSocketServer.java
@@ -0,0 +1,44 @@
+package server;
+import java.io.*;
+import javax.websocket.*;
+import javax.websocket.server.*;
+@ServerEndpoint("/websocket")
+public class WebSocketServer
+{
+    @OnClose
+    public void onClose(Session session,CloseReason closeReason)
+    {
+        System.out.println("WebSocket Server: onClose()");
+        System.out.println(closeReason);
+    }
+    @OnError
+    public void onError(Session session,Throwable throwable)
+    {
+        System.out.println("WebSocket Server: onError()");
+        throwable.printStackTrace();
+    }
+    @OnMessage
+    public void onMessage(String string,Session session)
+    {
+        System.out.println("WebSocket Server: onMessage()");
+        System.out.println("WebSocket Client: "+string);
+        try
+        {
+            sendText(session,true,string+"? OK!");
+        }
+        catch(IOException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+    @OnOpen
+    public void onOpen(Session session,EndpointConfig endpointConfig)
+    {
+        System.out.println("WebSocket Server: onOpen()");
+    }
+    private void sendText(Session session,boolean async,String string) throws IOException
+    {
+        if(async) session.getAsyncRemote().sendText(string);
+        else session.getBasicRemote().sendText(string);
+    }
+}
